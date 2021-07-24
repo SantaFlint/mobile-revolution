d596f11300786b46400cca538fd60923a6bab5f5
CherkasovAS <samael531010@gmail.com>
2021-07-24 12:32:03 +0900

    modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,55 @@
-const autoprefixer = require('autoprefixer');
-const csso = require('postcss-csso');
-const gulp = require('gulp');
-const less = require('gulp-less');
-const postcss = require('gulp-postcss');
-const rename = require('gulp-rename');
-const sync = require('browser-sync');
-const terser = require('gulp-terser');
-const plumber = require('gulp-plumber');
-const sourcemap = require('gulp-sourcemaps');
+"use strict";
+var autoprefixer = require('autoprefixer');
+var csso = require('postcss-csso');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var sync = require('browser-sync');
+var terser = require('gulp-terser');
+var plumber = require('gulp-plumber');
+var sourcemap = require('gulp-sourcemaps'); // Styles
 
-
-
-// Styles
-
-const styles = () => {
-    return gulp.src("source/less/style.less")
-        .pipe(plumber())
-        .pipe(sourcemap.init())
-        .pipe(less())
-        .pipe(postcss([
-            autoprefixer(),
-            csso,
-        ]))
-        .pipe(sourcemap.write("."))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest("source/css"))
-        .pipe(sync.stream());
-}
+var styles = function styles() {
+    return gulp.src("source/less/style.less").pipe(plumber()).pipe(sourcemap.init()).pipe(less()).pipe(postcss([autoprefixer(), csso])).pipe(sourcemap.write(".")).pipe(rename({
+        suffix: '.min'
+    })).pipe(gulp.dest("source/css")).pipe(sync.stream());
+};
 
 exports.styles = styles;
 
-const scripts = () => {
-    return gulp.src('source/js/script.js')
-        .pipe(terser())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('source/js'))
-        .pipe(sync.stream());
-}
+var scripts = function scripts() {
+    return gulp.src('source/js/script.js').pipe(terser()).pipe(rename({
+        suffix: '.min'
+    })).pipe(gulp.dest('source/js')).pipe(sync.stream());
+};
 
-exports.scripts = scripts;
+exports.scripts = scripts; // Server
+// const server = sync.create();
 
-// Server
-
-const server = sync.create();
-
-const reload = (done) => {
+var reload = function reload(done) {
     server.reload();
     done();
-}
+};
 
-const server = (done) => {
+var server = function server(done) {
     sync.init({
         ui: false,
         cors: true,
         notify: false,
         server: {
             baseDir: './source'
-        },
+        }
     });
     done();
-}
+};
 
-exports.server = server;
+exports.server = server; // Watcher
 
-// Watcher
-
-const watcher = () => {
+var watcher = function watcher() {
     gulp.watch('source/*.html', gulp.series(reload));
     gulp.watch('source/less/**/*.less', gulp.series(styles, reload));
     gulp.watch('source/js/script.js', gulp.series(scripts, reload));
-}
+};
 
-exports.default = gulp.series(
-    styles, scripts, server, watcher
-);
+exports["default"] = gulp.series(styles, scripts, server, watcher);
