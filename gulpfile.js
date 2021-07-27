36acddaf3e8e67a581a3b978b2ac53cc750b13ac
CherkasovAS <samael531010@gmail.com>
2021-07-27 19:58:22 +0900

    fix server in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,87 @@
-"use strict";
-var autoprefixer = require('autoprefixer');
-var csso = require('postcss-csso');
-var gulp = require('gulp');
-var less = require('gulp-less');
-var postcss = require('gulp-postcss');
-var rename = require('gulp-rename');
-var sync = require('browser-sync');
-var terser = require('gulp-terser');
-var plumber = require('gulp-plumber');
-var sourcemap = require('gulp-sourcemaps'); // Styles
+const autoprefixer = require('autoprefixer');
+const csso = require('postcss-csso');
+const gulp = require('gulp');
+const less = require('gulp-less');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const sync = require('browser-sync').create();
+const terser = require('gulp-terser');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
 
-var styles = function styles() {
-    return gulp.src("source/less/style.less").pipe(plumber()).pipe(sourcemap.init()).pipe(less()).pipe(postcss([autoprefixer(), csso])).pipe(sourcemap.write(".")).pipe(rename({
+// Styles
+
+const styles = function styles() {
+    return gulp.src("source/less/style.less")
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(less())
+      .pipe(postcss([
+        autoprefixer(),
+        csso
+      ]))
+      .pipe(sourcemap.write("."))
+      .pipe(rename({
         suffix: '.min'
-    })).pipe(gulp.dest("source/css")).pipe(sync.stream());
+    }))
+      .pipe(gulp.dest("source/css"))
+      .pipe(sync.stream());
 };
 
 exports.styles = styles;
+// Styles
+
+// const styles = () => {
+//   return gulp.src("source/less/style.less")
+//     .pipe(plumber())
+//     .pipe(sourcemap.init())
+//     .pipe(less())
+//     .pipe(postcss([
+//       autoprefixer()
+//     ]))
+//     .pipe(sourcemap.write("."))
+//     .pipe(gulp.dest("source/css"))
+//     .pipe(sync.stream());
+// }
+
+// exports.styles = styles;
 
 var scripts = function scripts() {
-    return gulp.src('source/js/script.js').pipe(terser()).pipe(rename({
+    return gulp.src('source/js/script.js')
+    .pipe(terser())
+    .pipe(rename({
         suffix: '.min'
-    })).pipe(gulp.dest('source/js')).pipe(sync.stream());
+    }))
+    .pipe(gulp.dest('source/js'))
+    .pipe(sync.stream());
 };
 
-exports.scripts = scripts; // Server
-// const server = sync.create();
+exports.scripts = scripts;
 
-var reload = function reload(done) {
-    server.reload();
-    done();
-};
 
-var server = function server(done) {
-    sync.init({
-        ui: false,
-        cors: true,
-        notify: false,
-        server: {
-            baseDir: './source'
-        }
-    });
-    done();
-};
+// Server
 
-exports.server = server; // Watcher
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
 
-var watcher = function watcher() {
-    gulp.watch('source/*.html', gulp.series(reload));
-    gulp.watch('source/less/**/*.less', gulp.series(styles, reload));
-    gulp.watch('source/js/script.js', gulp.series(scripts, reload));
-};
+exports.server = server;
 
-exports["default"] = gulp.series(styles, scripts, server, watcher);
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
+}
+
+exports.default = gulp.series(
+  styles, server, watcher
+);
